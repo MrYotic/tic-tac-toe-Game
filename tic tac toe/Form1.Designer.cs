2b3278c42ebbd89d0565254d4c5389b668e57ea9
Yotic <yoticcitoy@gmail.com>
2021-07-02 09:48:38 +0300

    Добавьте файлы проекта.

---
+++ tic tac toe/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace tic_tac_toe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        public void Controls_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button.Equals(MouseButtons.Left))
+            {
+                ((Control)sender).Capture = false;
+                var m = Message.Create(Handle, 0xa1, new IntPtr(0x2), IntPtr.Zero);
+                WndProc(ref m);
+            }
+        }
+
+        public void panels_Paint(object sender, PaintEventArgs e)
+        {
+            ControlPaint.DrawBorder(e.Graphics, _pic[((PictureBox)sender).TabIndex - 1000].ClientRectangle, Color.Silver, ButtonBorderStyle.Solid);
+        }
+
+        public void button1_Click(object sender, EventArgs e)
+        {
+            Process.GetCurrentProcess().Kill();
+        }
+
+        public void panel2_Paint(object sender, PaintEventArgs e)
+        {
+            ControlPaint.DrawBorder(e.Graphics, this.panel2.ClientRectangle, Color.Silver, ButtonBorderStyle.Solid);
+        }
+
+        public void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            ControlPaint.DrawBorder(e.Graphics, this.panel1.ClientRectangle, Color.Silver, ButtonBorderStyle.Solid);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(0, 26);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(397, 424);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(46, 51);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(300, 300);
+            this.panel2.TabIndex = 1;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Font = new System.Drawing.Font("Open Sans Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.Silver;
+            this.label1.Location = new System.Drawing.Point(41, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(235, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Сейчас ходит:  Крестик";
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.Silver;
+            this.button1.Location = new System.Drawing.Point(373, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(22, 22);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "X";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label2.Font = new System.Drawing.Font("Open Sans Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.Silver;
+            this.label2.Location = new System.Drawing.Point(41, 372);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(148, 26);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Счёт: x: 0 / o: 0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(397, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ tic tac toe/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tic_tac_toe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.MouseDown += Controls_MouseDown;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DrawingPanelBoxes();
+            variotes = new int[8, 3] { { 0, 1, 2 }, { 3, 4, 5 }, { 6, 7, 8}, { 0, 3, 6}, { 1, 4, 7}, { 2, 5, 8}, {0, 4, 8 }, { 6, 4, 2} };
+        }
+        PictureBox[] _pic = new PictureBox[9];
+        private void DrawingPanelBoxes()
+        {
+            for (int i = 0; i < _pic.Length; i++)
+            {
+                _pic[i] = new PictureBox();
+                _pic[i].BackColor = Color.White;
+                _pic[i].Size = new Size(100, 100);
+                _pic[i].Location = new Point( i / 3 * 100, i % 3 * 100);
+                _pic[i].TabIndex = 1000 + i;
+                _pic[i].Paint += panels_Paint;
+                _pic[i].Click += ClickPanel;
+            }
+            panel2.Controls.AddRange(_pic);
+        }
+
+        bool state = true; //true - X ; false - О
+        bool[,] states = new bool[9, 2];// занято / не занято || крестик / нолик
+        private void ClickPanel(object sender, EventArgs e)
+        {
+            if (!states[((PictureBox)sender).TabIndex - 1000, 0])
+            {
+                states[((PictureBox)sender).TabIndex - 1000, 0] = true;
+                states[((PictureBox)sender).TabIndex - 1000 , 1] = state;
+                if (state)
+                    DraweXtic(((PictureBox)sender).TabIndex - 1000);
+                else
+                    _pic[((PictureBox)sender).TabIndex - 1000].CreateGraphics().DrawEllipse(new Pen(Brushes.Silver, 3), 10, 10, 80, 80);
+                CheckWin(state);
+                state = !state;
+                label1.Text = state == true ? "Сейчас ходит: Крестик" : "Сейчас ходит: Нолик";
+            }
+            else
+                return;
+        }
+        int[,] variotes = new int[8, 3];
+        private void CheckWin(bool state2)
+        {
+            if (int.Parse(label2.Text[label2.Text.IndexOf("x: ") + 3].ToString()) > 8 || int.Parse(label2.Text[label2.Text.IndexOf("o: ") + 3].ToString()) > 8)
+                Process.GetCurrentProcess().Kill();
+
+            for (int i = 0; i < variotes.Length / 3; i++)
+                if (states[variotes[i, 0], 0] && states[variotes[i, 0], 1] == state2 && states[variotes[i, 1], 0] && states[variotes[i, 1], 1] == state2 && states[variotes[i, 2], 0] && states[variotes[i, 2], 1] == state2)
+                {
+                    label2.Text = state2 == true ? "Счёт: x: " + (int.Parse(label2.Text[label2.Text.IndexOf("x: ") + 3].ToString())+1) + " / o: " + label2.Text[label2.Text.IndexOf("o: ") + 3] : "Счёт: x: " + label2.Text[label2.Text.IndexOf("x: ") + 3] + " / o: " + (int.Parse(label2.Text[label2.Text.IndexOf("o: ") + 3].ToString()) + 1);
+                    states = new bool[9,2];
+                    for (int o = 0; o < _pic.Length; o++)
+                        _pic[o].Image = new Bitmap(100, 100);
+                }  
+        }
+        private void DraweXtic(int i)
+        {            
+            Bitmap bitmap = new Bitmap(100, 100);
+            for (int x = 10; x < bitmap.Width - 10 ; x++)
+                for (int y = 0; y < 3; y++)
+                {
+                    bitmap.SetPixel(x + y, x, Color.Silver);
+                    bitmap.SetPixel(x + y, bitmap.Width  - x, Color.Silver);
+                }
+            _pic[i].Image = bitmap;
+        }
+    }
+}
