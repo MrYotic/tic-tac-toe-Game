057dee6095bcaed4bc721b60baa226dae70393db
Yotic <yoticcitoy@gmail.com>
2021-07-05 03:43:44 +0300

    27 строк, пофиксил баг, теперь прорисовка фигур в 1 пиксел, а не в 3

--- tic tac toe/Form1.cs
+++ tic tac toe/Form1.cs
@@ -7,11 +7,11 @@ namespace tic_tac_toe{
         bool state = true;
         bool[,] states = new bool[9, 2];
         private void ClickPanel(object sender, EventArgs e){
-            if (states[((PictureBox)sender).TabIndex - 1000, 1] == null){
+            if (states[((PictureBox)sender).TabIndex - 1000, 0] == false){
                 states[((PictureBox)sender).TabIndex - 1000 , 0] = true;
                 states[((PictureBox)sender).TabIndex - 1000 , 1] = state;
                 if (state) DraweXtic(((PictureBox)sender).TabIndex - 1000, (Bitmap)_pic[((PictureBox)sender).TabIndex - 1000].Image);
-                else _pic[((PictureBox)sender).TabIndex - 1000].CreateGraphics().DrawEllipse(new Pen(Brushes.Silver, 3), 10, 10, 80, 80);
+                else _pic[((PictureBox)sender).TabIndex - 1000].CreateGraphics().DrawEllipse(new Pen(Brushes.Silver, 1), 10, 10, 80, 80);
                 if (int.Parse(label2.Text[label2.Text.IndexOf("x: ") + 3].ToString()) > 8 || int.Parse(label2.Text[label2.Text.IndexOf("o: ") + 3].ToString()) > 8) this.Close();
                 for (int i = 0; i < new int[8, 3] { { 0, 1, 2 }, { 3, 4, 5 }, { 6, 7, 8 }, { 0, 3, 6 }, { 1, 4, 7 }, { 2, 5, 8 }, { 0, 4, 8 }, { 6, 4, 2 } }.Length / 3; i++)
                     if (states[new int[8, 3] { { 0, 1, 2 }, { 3, 4, 5 }, { 6, 7, 8 }, { 0, 3, 6 }, { 1, 4, 7 }, { 2, 5, 8 }, { 0, 4, 8 }, { 6, 4, 2 } }[i, 0], 0] && states[new int[8, 3] { { 0, 1, 2 }, { 3, 4, 5 }, { 6, 7, 8 }, { 0, 3, 6 }, { 1, 4, 7 }, { 2, 5, 8 }, { 0, 4, 8 }, { 6, 4, 2 } }[i, 0], 1] == state && states[new int[8, 3] { { 0, 1, 2 }, { 3, 4, 5 }, { 6, 7, 8 }, { 0, 3, 6 }, { 1, 4, 7 }, { 2, 5, 8 }, { 0, 4, 8 }, { 6, 4, 2 } }[i, 1], 0] && states[new int[8, 3] { { 0, 1, 2 }, { 3, 4, 5 }, { 6, 7, 8 }, { 0, 3, 6 }, { 1, 4, 7 }, { 2, 5, 8 }, { 0, 4, 8 }, { 6, 4, 2 } }[i, 1], 1] == state && states[new int[8, 3] { { 0, 1, 2 }, { 3, 4, 5 }, { 6, 7, 8 }, { 0, 3, 6 }, { 1, 4, 7 }, { 2, 5, 8 }, { 0, 4, 8 }, { 6, 4, 2 } }[i, 2], 0] && states[new int[8, 3] { { 0, 1, 2 }, { 3, 4, 5 }, { 6, 7, 8 }, { 0, 3, 6 }, { 1, 4, 7 }, { 2, 5, 8 }, { 0, 4, 8 }, { 6, 4, 2 } }[i, 2], 1] == state){
@@ -21,8 +21,7 @@ namespace tic_tac_toe{
                 state = !state;
                 label1.Text = state == true ? "Сейчас ходит: Крестик" : "Сейчас ходит: Нолик";}}
         private void DraweXtic(int i, Bitmap bitmap){
-            for (int x = 10; x < bitmap.Width - 10 ; x++)
-                for (int y = 0; y < 3; y++){
-                    bitmap.SetPixel(x + y, x, Color.Silver);
-                    bitmap.SetPixel(x + y, bitmap.Width  - x, Color.Silver);}
+            for (int x = 0; x < bitmap.Width - 20 ; x++){
+                bitmap.SetPixel(bitmap.Width - 10 - x, bitmap.Height - 10 - x, Color.Silver);
+                bitmap.SetPixel( x + 10, bitmap.Width - x - 10, Color.Silver);}
             _pic[i].Image = bitmap;}}}
