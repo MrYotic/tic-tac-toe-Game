3a503a30fcbfaadef3e7d4916e0d8c5a48441e0b
Yotic <55879406+MrYotic@users.noreply.github.com>
2021-12-16 13:05:32 +0300

    Wtf

--- tic tac toe/Form1.cs
+++ tic tac toe/Form1.cs
@@ -5,8 +5,7 @@ namespace a{
         private void aa1(object a1a, System.EventArgs e){
             if (aaa[((System.Windows.Forms.PictureBox)a1a).TabIndex - 1000, 0] == false){
                 aaa[((System.Windows.Forms.PictureBox)a1a).TabIndex - 1000 , 0] = aaa[((System.Windows.Forms.PictureBox)a1a).TabIndex - 1000 , 1] = true;
-                if (a1[0,0]) a11a[((System.Windows.Forms.PictureBox)a1a).TabIndex - 1000].Image = a111(((System.Windows.Forms.PictureBox)a1a).TabIndex - 1000, (System.Drawing.Bitmap)a11a[((System.Windows.Forms.PictureBox)a1a).TabIndex - 1000].Image);
-                else a11a[((System.Windows.Forms.PictureBox)a1a).TabIndex - 1000].CreateGraphics().DrawEllipse(new System.Drawing.Pen(System.Drawing.Brushes.Silver, 1), 10, 10, 80, 80);
+                if (a1[0,0]) new List<Rectangle>{new Rectangle(10, 10, 80, 80), new Rectangle(10, 80, 80, 10)}.ForEach(x => a11a[((System.Windows.Forms.PictureBox)a1a).TabIndex - 1000].CreateGraphics().DrawLine(new System.Drawing.Pen(System.Drawing.Brushes.Silver, 1), x.left, x.top, x.right, x.bottom)); else a11a[((System.Windows.Forms.PictureBox)a1a).TabIndex - 1000].CreateGraphics().DrawEllipse(new System.Drawing.Pen(System.Drawing.Brushes.Silver, 1), 10, 10, 80, 80);
                 if (int.Parse(a11.Text[a11.Text.IndexOf("x: ") + 3].ToString()) > 8 || int.Parse(a11.Text[a11.Text.IndexOf("o: ") + 3].ToString()) > 8) this.Close();
                 for (int i = 0; i < new int[8, 3] { { 0, 1, 2 }, { 3, 4, 5 }, { 6, 7, 8 }, { 0, 3, 6 }, { 1, 4, 7 }, { 2, 5, 8 }, { 0, 4, 8 }, { 6, 4, 2 } }.Length / 3; i++)
                     if (aaa[new int[8, 3] { { 0, 1, 2 }, { 3, 4, 5 }, { 6, 7, 8 }, { 0, 3, 6 }, { 1, 4, 7 }, { 2, 5, 8 }, { 0, 4, 8 }, { 6, 4, 2 } }[i, 0], 0] && aaa[new int[8, 3] { { 0, 1, 2 }, { 3, 4, 5 }, { 6, 7, 8 }, { 0, 3, 6 }, { 1, 4, 7 }, { 2, 5, 8 }, { 0, 4, 8 }, { 6, 4, 2 } }[i, 0], 1] == a1 && aaa[new int[8, 3] { { 0, 1, 2 }, { 3, 4, 5 }, { 6, 7, 8 }, { 0, 3, 6 }, { 1, 4, 7 }, { 2, 5, 8 }, { 0, 4, 8 }, { 6, 4, 2 } }[i, 1], 0] && aaa[new int[8, 3] { { 0, 1, 2 }, { 3, 4, 5 }, { 6, 7, 8 }, { 0, 3, 6 }, { 1, 4, 7 }, { 2, 5, 8 }, { 0, 4, 8 }, { 6, 4, 2 } }[i, 1], 1] == a1 && aaa[new int[8, 3] { { 0, 1, 2 }, { 3, 4, 5 }, { 6, 7, 8 }, { 0, 3, 6 }, { 1, 4, 7 }, { 2, 5, 8 }, { 0, 4, 8 }, { 6, 4, 2 } }[i, 2], 0] && aaa[new int[8, 3] { { 0, 1, 2 }, { 3, 4, 5 }, { 6, 7, 8 }, { 0, 3, 6 }, { 1, 4, 7 }, { 2, 5, 8 }, { 0, 4, 8 }, { 6, 4, 2 } }[i, 2], 1] == a1[0,0]){
@@ -14,9 +13,4 @@ namespace a{
                         aaa = new bool[9, 2];
                         for (int o = 0; o < a11a.Length; o++) a11a[o].Image = new System.Drawing.Bitmap(100, 100);}
                 a1[0,0] = !a1[0,0];
-                aaaa.Text = a1[0,0] == true ? "Сейчас ходит: Крестик" : "Сейчас ходит: Нолик";}}
-        private Bitmap a111(int i, System.Drawing.Bitmap a1aa){
-            for (int x = 0; x < a1aa.Width - 20 ; x++){
-                a1aa.SetPixel(a1aa.Width - 10 - x, a1aa.Height - 10 - x, System.Drawing.Color.Silver);
-                a1aa.SetPixel( x + 10, a1aa.Width - x - 10, System.Drawing.Color.Silver);}
-             }return a1aa;}}
+                aaaa.Text = a1[0,0] == true ? "Сейчас ходит: Крестик" : "Сейчас ходит: Нолик";}}}
